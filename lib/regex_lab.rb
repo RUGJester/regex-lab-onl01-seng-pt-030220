32a40ecb4fb8533eb035fd94219c455a7158fb84
Sean Melton <seanlape@gmail.com>
2020-03-12 23:02:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,33 @@
-(/^[A-Z].+[\.!?]$/)
-puts "The rain in Spain lies mainly in the plain".scan(/\w+ain/)
-puts "///"
-puts "The rain in Spain lies mainly in the plain".match(/\w+ain/)
-puts "///"
-
-string = "This is the  sentence to do it 1234 6954"
-
 def starts_with_a_vowel?(word)
-  word.match(/\w [aeiouy]/)? true : false
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
-
-starts_with_a_vowel?(string)
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/\un\w+ing\b/)
+  new_text_array = []
+  original_string = text.split(" ")
+  original_string.each do |word|
+    if word.match(/(^un)|(ing$)/)
+      new_text_array.push(word)
+    end
+  end
+  return new_text_array
 end
-words_starting_with_un_and_ending_with_ing(string)
+
+def words_five_letters_long(text)
+  new_text_array = []
+  original_string = text.split(" ")
+  original_string.each do |word|
+  if word.match(/^[a-zA-Z]{5}$/)
+    new_text_array.push(word)
+  end
+end
+return new_text_array
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/^[A-Z]/) && text.match(/[ _.,!"']$/) ? true : false
+end
+
+def valid_phone_number?(phone)
+  phone.match(/\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}/) ? true : false
+end
