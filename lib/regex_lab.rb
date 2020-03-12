20957ef7f8ba60cf18a9c8bdbab77aae2d647761
Sean Melton <seanlape@gmail.com>
2020-03-12 23:01:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,1 +1,18 @@
+(/^[A-Z].+[\.!?]$/)
+puts "The rain in Spain lies mainly in the plain".scan(/\w+ain/)
+puts "///"
+puts "The rain in Spain lies mainly in the plain".match(/\w+ain/)
+puts "///"
 
+string = "This is the  sentence to do it 1234 6954"
+
+def starts_with_a_vowel?(word)
+  word.match(/\w [aeiouy]/)? true : false
+end
+
+starts_with_a_vowel?(string)
+
+def words_starting_with_un_and_ending_with_ing(text)
+ text.scan(/\un\w+ing\b/)
+end
+words_starting_with_un_and_ending_with_ing(string)
